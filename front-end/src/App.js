c80687f667a75130d933f110755ef54828daa3cd
Noah King <noah.h.king@gmail.com>
2020-07-07 19:58:48 -0700

    added a few features to the home page but realize it would be silly to build it out without somw data or at least knowing how to data is coming in

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,11 +1,30 @@
-import React, { Component } from 'react';
-import './App.css';
-import NavBar from './components/NavBar'
+import React, { Component } from "react"
+import "./App.css"
+import NavBar from "./components/NavBar"
+import HomePage from "./components/HomePage"
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom"
 
 function App() {
   return (
     <div>
       <NavBar />
+      <Router>
+        <Route exact path="/">
+          <HomePage />
+        </Route>
+        <Route exact path="/roadmap">
+          Roadmap
+        </Route>
+        <Route exact path="/profile">
+          Profile
+        </Route>
+        <Route exact path="/community">
+          Community
+        </Route>
+        <Route exact path="/support">
+          Support
+        </Route>
+      </Router>
     </div>
   )
 }
